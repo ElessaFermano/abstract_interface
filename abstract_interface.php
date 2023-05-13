bf71dd1e5eac9150e918777081dcd3c5a1a96f81
elessafermano <elessamaefermano@gmail.com>
2023-05-13 13:03:41 +0800

    jj

--- abstract_interface.php
+++ abstract_interface.php
@@ -6,22 +6,25 @@ abstract class Database
     public $sname='localhost';
     public $uname='root';
     public $password='';
-    public $dbname='fermano';
+    public $dbname = 'fermano';
     
     public function __construct($conn){
         $this->conn = new mysqli($this->sname, $this->uname, $this->password);
+        $db = "CREATE DATABASE IF NOT EXISTS $this->dbname";
+        $this->conn->query($db);
 
+        var_dump($db);
     }
 
     abstract public function insert() : string;
     
 }
 
-class Students extends Database
-{
-    public function insert()
-    {
+// class Students extends Database
+// {
+//     public function insert()
+//     {
+//         return "Elessa";
+//     }
 
-    }
-
-}
+// }
